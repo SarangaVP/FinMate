8e2f2087bd1231c8b8d253cb205b30297d022e76
Saranga Malshan <sarangavp36@gmail.com>
2026-01-24 01:52:36 +0530

    Implement auth routes for user registration and login

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const connectDB = require('./src/config/db');
+const authRoutes = require('./src/routes/authRoutes');
 
 connectDB();
 
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000; 
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.status(200).json({ 
